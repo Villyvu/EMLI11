f940c4c5911c513ec510f81966d043ebeadcddd2
yonraf <55747457+yonraf@users.noreply.github.com>
2023-06-05 13:08:56 +0200

    1 instead of pump

--- moisture_check.sh
+++ moisture_check.sh
@@ -4,5 +4,5 @@ url="http://192.168.10.185/led/a/yellow/status"
 response=$(curl "$url")
 
 if [ "$response" -eq 1 ]; then
-	mosquitto_pub -h localhost -p 1883 -u my_user -P embeddedlinux -t my_user/command/pump -m "pump"
+	mosquitto_pub -h localhost -p 1883 -u my_user -P embeddedlinux -t my_user/command/pump -m "1"
 fi
